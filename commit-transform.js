587903be48c7fe9cbf17277e18e4dab7d81b533d
Universal Studio <10200399+TMUniversal@users.noreply.github.com>
2023-07-18 07:08:51 +0200

    style: run prettier

--- commit-transform.js
+++ commit-transform.js
@@ -1,6 +1,6 @@
-'use strict';
+"use strict";
 
-const types = require('./types');
+const types = require("./types");
 
 const COMMIT_HASH_LENGTH = 7;
 
@@ -13,34 +13,40 @@ const COMMIT_HASH_LENGTH = 7;
  */
 module.exports = (commit, context) => {
 	if (commit.notes) {
-		commit.notes.forEach(note => {
-			note.title = 'Breaking changes';
+		commit.notes.forEach((note) => {
+			note.title = "Breaking changes";
 		});
 	}
 
-	if (types.types[commit.type] && (types.types[commit.type].changelog || (commit.notes && commit.notes.length > 0))) {
-		commit.groupType = `${types.types[commit.type].emoji ? `${types.types[commit.type].emoji} ` : ''}${
-			types.types[commit.type].title
-		}`;
+	if (
+		types.types[commit.type] &&
+		(types.types[commit.type].changelog ||
+			(commit.notes && commit.notes.length > 0))
+	) {
+		commit.groupType = `${
+			types.types[commit.type].emoji ? `${types.types[commit.type].emoji} ` : ""
+		}${types.types[commit.type].title}`;
 	} else {
 		return null;
 	}
 
-	if (commit.scope === '*') {
-		commit.scope = '';
+	if (commit.scope === "*") {
+		commit.scope = "";
 	}
 
-	if (typeof commit.hash === 'string') {
+	if (typeof commit.hash === "string") {
 		commit.shortHash = commit.hash.slice(0, COMMIT_HASH_LENGTH);
 	}
 
 	const references = [];
 
-	if (typeof commit.subject === 'string') {
-		let url = context.repository ? `${context.host}/${context.owner}/${context.repository}` : context.repoUrl;
+	if (typeof commit.subject === "string") {
+		let url = context.repository
+			? `${context.host}/${context.owner}/${context.repository}`
+			: context.repoUrl;
 
 		if (url) {
-			url += '/issues/';
+			url += "/issues/";
 			// Issue URLs.
 			commit.subject = commit.subject.replace(/#(\d+)/g, (_, issue) => {
 				references.push(issue);
@@ -50,13 +56,16 @@ module.exports = (commit, context) => {
 
 		if (context.host) {
 			// User URLs.
-			commit.subject = commit.subject.replace(/\B@([a-z0-9](?:-?[a-z0-9]){0,38})/g, `[@$1](${context.host}/$1)`);
+			commit.subject = commit.subject.replace(
+				/\B@([a-z0-9](?:-?[a-z0-9]){0,38})/g,
+				`[@$1](${context.host}/$1)`,
+			);
 		}
 	}
 
 	if (commit.references) {
 		// Remove references that already appear in the subject
-		commit.references = commit.references.filter(reference => {
+		commit.references = commit.references.filter((reference) => {
 			if (!references.includes(reference.issue)) {
 				return true;
 			}
